ad52216b68421c1afc305b127e611a61e191f3ee
poludnev <poludnev.roman@gmail.com>
2022-08-01 01:27:17 +0300

    Adaptivated HowItGoes

--- src/components/sections/HowItGoes/HowItGoes.jsx
+++ src/components/sections/HowItGoes/HowItGoes.jsx
@@ -6,6 +6,7 @@ export const HowItGoes = () => {
   const { t } = useTranslation();
   return (
     <div className={styles.container}>
+      <div className={styles.bgfade} />
       <h2 className={styles.title}>
         {t('howItGoes.title.l1')}
         <br />
@@ -13,14 +14,14 @@ export const HowItGoes = () => {
       </h2>
       <div className={styles.box}>
         <div className={styles.step}>
-          <div className={styles.number}>1.</div>
+          <div className={styles.number}>1</div>
           <div className={styles.icon}>
             <img className={styles.icon1} src="public/images/icon-1.png" alt="icon" />
           </div>
           <p>{t('howItGoes.p1')}</p>
         </div>
         <div className={styles.step}>
-          <div className={styles.number}>2.</div>
+          <div className={styles.number}>2</div>
           <div className={styles.icon}>
             <img className={styles.icon2} src="public/images/icon-2.png" alt="icon" />
           </div>
@@ -29,7 +30,7 @@ export const HowItGoes = () => {
         </div>
 
         <div className={styles.step}>
-          <div className={styles.number}>3.</div>
+          <div className={styles.number}>3</div>
           <div className={styles.icon}>
             <img className={styles.icon3} src="public/images/icon-3.png" alt="icon" />
           </div>
