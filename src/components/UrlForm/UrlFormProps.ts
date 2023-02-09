558a296cecb5857a6b9381c8e1aac8ca72e58d0d
poludnev <poludnev.roman@gmail.com>
2023-02-10 01:48:19 +0300

    add form and url validation

--- src/components/UrlForm/UrlFormProps.ts
+++ src/components/UrlForm/UrlFormProps.ts
@@ -1,1 +1,8 @@
-export interface IUrlFormProps { children?: React.ReactNode };
+import React from 'react';
+
+export interface IUrlFormProps { 
+  children?: React.ReactNode;
+  onSubmit: (url: string) => void | Promise<void>;
+  inline?: boolean;
+  
+};
