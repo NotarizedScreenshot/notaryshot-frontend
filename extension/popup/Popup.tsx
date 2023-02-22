e638606739340dda8369566658eb5f11371c2ac4
poludnev <poludnev.roman@gmail.com>
2023-02-23 02:27:08 +0300

    fixed extension button possition, revised layout

--- extension/popup/Popup.tsx
+++ extension/popup/Popup.tsx
@@ -1,10 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-
+import * as yup from 'yup';
 import './popup.css';
 
 const Popup: React.FC = () => {
+  const [urlInputValue, setUrlInputValue] = useState<string>('');
+  const [validating, setValidating] = useState<boolean>(false);
+  const [isInvalid, setInvalid] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>('');
+  const [dirtry, setDirty] = useState<boolean>(false);
+  const [submitting, setSubmitting] = useState<boolean>(false);
+
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const urlValidationSchema = { url: yup.string().required().url() };
+  const validate = (url: string) => yup.object().shape(urlValidationSchema).validate({ url });
+
+  const buttonHandler = () => {
+    chrome.runtime.sendMessage({ url: null }, (response) => {
+      console.log('button handler sendMessage response: ', response);
+    });
+  };
+
   const notarizeHandler: React.MouseEventHandler<HTMLButtonElement> = (event) => {
     event.preventDefault();
 
@@ -14,30 +31,85 @@ const Popup: React.FC = () => {
       });
     });
   };
-  const submitHandler: React.MouseEventHandler<HTMLButtonElement> = (event) => {
-    event.preventDefault();
-    if (!!inputRef.current) {
-      chrome.runtime.sendMessage({ url: inputRef.current.value }, (response) => {
-        console.log('button handler sendMessage response: ', response);
-      });
+
+  const changeInputHandler: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setUrlInputValue(event.currentTarget.value);
+    if (dirtry) {
+      validate(event.currentTarget.value)
+        .then((data) => {
+          setInvalid(false);
+          setError(null);
+          setValidating(false);
+        })
+        .catch((error: yup.ValidationError) => {
+          setInvalid(true);
+          setError(error.message);
+        });
     }
   };
 
+  const submitHandler: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    setDirty(true);
+    setValidating(true);
+    validate(urlInputValue)
+      .then(() => {
+        setValidating(false);
+        setInvalid(false);
+        setSubmitting(true);
+
+        chrome.runtime.sendMessage({ url: urlInputValue }, (response) => {
+          console.log('button handler sendMessage response: ', response);
+          setSubmitting(false);
+        });
+      })
+      .catch((error: yup.ValidationError) => {
+        setInvalid(true);
+        setError(error.message);
+        inputRef.current?.focus();
+      });
+  };
+
   return (
-    <div>
-      <h1>Notarize everything!</h1>
-      <div>
-        <h3>Press button to notorize current page</h3>
+    <div className='container'>
+      <div className='background'></div>
+      <div className='icon-container' onClick={buttonHandler}>
+        <img src='icon-header.png' alt='icon' className='icon-container__icon' />
+      </div>
+      <h1 className='h1'>Save your evidence</h1>
+      <div className='send-url-container'>
+        <h3 className='h3'>Submit a url of a resource you want to be saved forever:</h3>
+        <form className='send-url-container_form' onSubmit={submitHandler}>
+          <label htmlFor='url' hidden={true}>
+            URL
+          </label>
+          <input
+            className='send-url-container_input'
+            ref={inputRef}
+            name='url'
+            autoComplete='off'
+            placeholder='url'
+            value={urlInputValue}
+            onChange={changeInputHandler}
+          ></input>
+          <button
+            type='submit'
+            className='btn-notarize'
+            aria-label='submit-button'
+            disabled={validating || isInvalid || submitting}
+          >
+            Submit
+          </button>
+          <div className='send-url-container_error' hidden={!isInvalid}>
+            {error}
+          </div>
+        </form>
+      </div>
+      <div className='send-url-container'>
+        <h3 className='h3'>Press button to notorize current page</h3>
         <div>
           <button onClick={notarizeHandler} className='btn-notarize'>
             Notarize current page
-          </button>
-        </div>
-        <h3>Or submit a url of a resource you want to be saved forever:</h3>
-        <div>
-          <input ref={inputRef} placeholder='url'></input>
-          <button onClick={submitHandler} className='btn-notarize'>
-            Submit
           </button>
         </div>
       </div>
