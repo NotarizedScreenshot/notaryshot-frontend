0744fed44188f4cd2a9ec3c842fc30fce80ffd09
poludnev <poludnev.roman@gmail.com>
2023-02-22 11:35:31 +0300

    fix message listenner error

--- extension/background/background.ts
+++ extension/background/background.ts
@@ -1,15 +1,16 @@
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  chrome.tabs.query({ active: true }, (tabs) => {
+  chrome.tabs.query({ active: true }).then((tabs) => {
     const activeTabIndex = tabs[0].index ?? 0;
-    console.log(msg);
     if (!!msg.url) {
-      chrome.tabs.create(
-        {
+      chrome.tabs
+        .create({
           url: `https://quantumoracle.app?url=${msg.url}`,
           index: activeTabIndex + 1,
-        },
-        () => sendResponse('Service worker response: message handled'),
-      );
+        })
+        .then(() => {
+          sendResponse('Service worker response: url handled');
+        });
     }
   });
+  return true;
 });
