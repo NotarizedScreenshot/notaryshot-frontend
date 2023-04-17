b468e58bd6201232f089c0ed2773bee5893ac313
poludnev <poludnev.roman@gmail.com>
2023-04-17 22:08:58 +0300

    add spy

--- src/pages/Preview/Preview.tsx
+++ src/pages/Preview/Preview.tsx
@@ -7,15 +7,24 @@ export const Preview: React.FC<IPreviewProps> = () => {
     <div className={styles.container}>
       <Header />
       <main className={styles.main}>
+        <div className={styles.bgCircles}></div>
+        <div className={styles.spy}>
+          <img src='/images/spy.png' alt='spy'></img>
+        </div>
+        <div className={styles.message}>
+          <div className={styles.text}>
+            If you click on «Notarize» your data will be saved forever, show off your NFT like a real secret agent!
+          </div>
+        </div>
         <CustomForm />
         <h2 className={styles.h2}>Confirm Verification</h2>
         <TweetResults />
         <MetadataPreview title='Http headers meta'>
           <HTTPMetadata />
-          </MetadataPreview>
+        </MetadataPreview>
         <MetadataPreview title='DNS headers meta'>
           <DNSMetadata />
-          </MetadataPreview>
+        </MetadataPreview>
       </main>
     </div>
   );
