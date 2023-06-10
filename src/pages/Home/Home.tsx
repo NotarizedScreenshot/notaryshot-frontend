fd95449f04834d7b20105e28d88d2500ad140aca
poludnev <poludnev.roman@gmail.com>
2023-06-10 21:56:50 +0200

    stab mobile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,22 +3,32 @@ import { Header, Hero, TryIt, HowItWorks, Benefits, Footer, Collapsable, Process
 import styles from './Home.module.scss';
 
 export const Home: React.FC<IHomeProps> = () => {
+  const windowWidth = window.innerWidth;
+  console.log(windowWidth);
   return (
     <div className={styles.container}>
-      <div className={styles.background}>
-        <div className={styles.gradientBlack} />
-      </div>
-      <div className={styles.content}>
-        <Header />
-        <Hero />
-        <TryIt />
-        <HowItWorks />
-        <Collapsable title='Detailed history'>
-          <Process />
-        </Collapsable>
-        <Benefits />
-        <Footer />
-      </div>
+      {windowWidth > 1200 ? (
+        <>
+          <div className={styles.background}>
+            <div className={styles.gradientBlack} />
+          </div>
+          <div className={styles.content}>
+            <Header />
+            <Hero />
+            <TryIt />
+            <HowItWorks />
+            <Collapsable title='Detailed history'>
+              <Process />
+            </Collapsable>
+            <Benefits />
+            <Footer />
+          </div>
+        </>
+      ) : (
+        <div className='nomobile'>
+          <h1>mobile version coming soon!</h1>
+        </div>
+      )}
     </div>
   );
 };
