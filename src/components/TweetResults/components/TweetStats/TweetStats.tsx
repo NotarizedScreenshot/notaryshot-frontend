fe26031141605f6e301643e2b0259c0220d3c839
poludnev <poludnev.roman@gmail.com>
2023-07-27 13:50:54 +0200

    Layout fixings

--- src/components/TweetResults/components/TweetStats/TweetStats.tsx
+++ src/components/TweetResults/components/TweetStats/TweetStats.tsx
@@ -7,37 +7,38 @@ export const TweetStats: React.FC<ITweetStatsProps> = ({
   views_count,
   bookmark_count,
 }) => {
+  console.log(favorite_count, quote_count, retweet_count, views_count, bookmark_count);
   return (
     <div className={styles.container}>
       <div className={styles.stat}>
         <div className={styles.icon}>
           <img src='images/Icon-likes.png' alt='like icon' />
         </div>
-        {favorite_count}
+        {!favorite_count ? '0' : favorite_count}
       </div>
       <div className={styles.stat}>
         <div className={styles.icon}>
           <img src='images/Icon-retweets.png' alt='like icon' />
         </div>
-        {retweet_count}
+        {!retweet_count ? '0' : retweet_count}
       </div>
       <div className={styles.stat}>
         <div className={styles.icon}>
           <img src='images/Icon-comments.png' alt='like icon' />
         </div>
-        {quote_count}
+        {!quote_count ? '0' : quote_count}
       </div>
       <div className={styles.stat}>
         <div className={styles.icon}>
           <img src='images/Icon-views.png' alt='like icon' />
         </div>
-        {views_count}
+        {!views_count ? '0' : views_count}
       </div>
       <div className={styles.stat}>
         <div className={styles.icon}>
           <img src='images/Icon-bookmarks.png' alt='like icon' />
         </div>
-        {bookmark_count}
+        {!bookmark_count ? '0' : bookmark_count}
       </div>
     </div>
   );
